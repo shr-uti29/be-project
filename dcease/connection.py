ccc9998504721f0cc86b85b6434328fafc2309a4
sarveshnavare7 <sarveshnavare7@gmail.com>
2022-04-14 00:53:46 +0530

    now i can handle exchange of texts

--- dcease/connection.py
+++ dcease/connection.py
@@ -3,10 +3,10 @@ from pickle import TRUE
 from owlready2 import *
 from owlready2.sparql.endpoint import *
 
-# onto_path.append("D:/Sem 7/project/ontology versions")
-# onto=get_ontology("D:/Sem 7/project/ontology versions/latestlatestlatest.owl")
-onto_path.append("C:/Users/shruti/Downloads/majorproject-master/majorproject-master")
-onto=get_ontology("C:/Users/shruti/Downloads/majorproject-master/majorproject-master/latestlatestlatest.owl")
+onto_path.append("D:/Sem 7/project/ontology versions")
+onto=get_ontology("D:/Sem 7/project/ontology versions/latestlatestlatest.owl")
+# onto_path.append("C:/Users/shruti/Downloads/majorproject-master/majorproject-master")
+# onto=get_ontology("C:/Users/shruti/Downloads/majorproject-master/majorproject-master/latestlatestlatest.owl")
  
 onto.load()
 
--- dcease/consumers.py
+++ dcease/consumers.py
@@ -6,35 +6,41 @@ from .connection import *
 from owlready2 import *
 from owlready2.sparql.endpoint import *
 
-# onto_path.append("D:/Sem 7/project/ontology versions")
-# onto=get_ontology("D:/Sem 7/project/ontology versions/latestlatestlatest.owl")
-onto_path.append("C:/Users/shruti/Downloads/majorproject-master/majorproject-master")
-onto=get_ontology("C:/Users/shruti/Downloads/majorproject-master/majorproject-master/latestlatestlatest.owl")
+onto_path.append("D:/Sem 7/project/ontology versions")
+onto=get_ontology("D:/Sem 7/project/ontology versions/latestlatestlatest.owl")
+# onto_path.append("C:/Users/shruti/Downloads/majorproject-master/majorproject-master")
+# onto=get_ontology("C:/Users/shruti/Downloads/majorproject-master/majorproject-master/latestlatestlatest.owl")
  
 onto.load()
-
 class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
         # if text_data=='TESTEST':
         #     self.send(json.dumps({'message':p}))
-        l=[]
-        l,p=disease('AtlanticCodAllergy')
-        print(l)
-        k=list(set(l))
-        m='untitled-ontology-19.'+p
-        if m in k:
-            k.remove(m)
-        print(p)
-        # for i in range(0,len(l)):
-        #     s=l[i]
-        self.send(json.dumps({'message':p,'message1':k}))
         # self.send(json.dumps({'message1':l}))
         # for i in range(1000):
         #     self.send(json.dumps({'message':randint(1,100)}))
         #     sleep(1)
 
     def receive(self,text_data):
+        text_data=text_data[1:len(text_data)-1]
         print(text_data)
-        if text_data=='yesno':
-            self.send(json.dumps({'message':"WORKS MDFKR"}))
+        if text_data=="ABCDSyndrome":
+            l=[]
+            l,p=disease('ABCDSyndrome')
+            k=list(set(l))
+            m='untitled-ontology-19.'+p
+            if m in k:
+                k.remove(m)
+            self.send(json.dumps({'message':p,'message1':k}))
+            sleep(5)
+        elif text_data=="AMEDSyndrome":
+            l=[]
+            l,p=disease('AMEDSyndrome')
+            k=list(set(l))
+            m='untitled-ontology-19.'+p
+            if m in k:
+                k.remove(m)
+            self.send(json.dumps({'message':p,'message1':k}))
+        else:
+            self.close()
